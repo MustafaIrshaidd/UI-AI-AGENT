90cb2484d9de3aa6044b6725852558b0bc22a1db
mustafairshaidd <mustafa-irshaid_2001@hotmail.com>
2025-08-05 22:22:29 +0300

    allow frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
-# fastapi_backend/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
+
+# Replace this with your actual frontend URL
+frontend_origin = os.environ.get("FRONT_END_URL")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[frontend_origin],  # Only allow your frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
