46c293bdd01567161a5a96d534f3360dbc76f250
mustafairshaidd <mustafa-irshaid_2001@hotmail.com>
2025-08-05 22:14:15 +0300

    test api

---
+++ frontend/components/HomeTest/HomeTest.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function HomeTest() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/some-endpoint`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from API:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
